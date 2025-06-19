007f75efe259bbd949b41ea41bc846e98b309859
Jake Faulkner <jakefaulkn@gmail.com>
2025-06-19 13:23:06 +1200

    call correct method

--- visualisation/plot_ts.py
+++ visualisation/plot_ts.py
@@ -422,7 +422,7 @@ def render_single_frame(
     )
 
     # Add the actual data for this frame
-    ground_motion_magnitude = xyts_file.get_tslice(frame_index)
+    ground_motion_magnitude = xyts_file.tslice_get(frame_index)
     current_data = ground_motion_magnitude[frame_index, :, :]
     pcm = ax.pcolormesh(
         xr,
