f1b835f84f298c8c80f99d7e98706f53f4ec2130
Jake Faulkner <jake.faulkner@canterbury.ac.nz>
2026-01-27 09:26:10 +1300

    feat(plot-response-spectra): add scenario station comparison plots

--- visualisation/waveforms/plot_response_spectra.py
+++ visualisation/waveforms/plot_response_spectra.py
@@ -15,6 +15,18 @@ from qcore import cli
 app = typer.Typer()
 
 
+def setup_plot_styling(
+    ax: Axes, component: str, ymax: float | None, ymin: float | None
+) -> None:
+    ax.set_ylabel(f"pSA [{component}, g]")
+    ax.set_xlabel("Period [s]")
+    ax.set_xscale("log")
+    ax.set_yscale("log")
+    ax.grid(visible=True, which="both", axis="both", lw=0.3)
+    if ymin is not None or ymax is not None:
+        ax.set_ylim(bottom=ymin, top=ymax)
+
+
 def plot_spectra(
     fig: Figure,
     ax: Axes,
@@ -23,6 +35,7 @@ def plot_spectra(
     component: str,
     ymax: float | None = None,
     ymin: float | None = None,
+    labels: list[str] | None = None,
 ) -> None:
     """Plot a spectra from a simulation.
 
@@ -39,28 +52,19 @@ def plot_spectra(
     ymin : float or None
         Min limit for y-axis.
     """
-    ax.set_ylabel(f"pSA [{component}, g]")
-    ax.set_xlabel("Period [s]")
-    ax.set_xscale("log")
-    ax.set_yscale("log")
-    ax.grid(visible=True, which="both", axis="both", lw=0.3)
-    if ymin is not None or ymax is not None:
-        ax.set_ylim(bottom=ymin, top=ymax)
-
-    for station in stations:
+    labels = labels or stations
+    for label, station in zip(labels, stations):
         spectra = dataset.pSA.sel(station=station, component=component).values
         periods = dataset.period.values
 
-        ax.plot(periods, spectra, label=station)
-
-    if len(stations) > 1:
-        ax.legend()
+        ax.plot(periods, spectra, label=label)
 
 
 @cli.from_docstring(app)
 def plot_spectra_cli(
-    dataset_path: Annotated[Path, typer.Argument()],
-    stations: Annotated[list[str], typer.Argument()],
+    dataset_paths: Annotated[list[Path], typer.Argument()],
+    scenarios: Annotated[list[str] | None, typer.Option("--scenario")] = None,
+    stations: Annotated[list[str] | None, typer.Option("--station")] = None,
     title: str | None = None,
     save: Path | None = None,
     dpi: int = 300,
@@ -81,18 +85,41 @@ def plot_spectra_cli(
     units : Units
         The units to plot in.
     """
-    dset = xr.open_dataset(dataset_path, engine="h5netcdf")
+    if not stations:
+        raise ValueError("Require at least one station to plot.")
+    elif len(dataset_paths) > 1 and (
+        scenarios is None or len(scenarios) != len(dataset_paths)
+    ):
+        raise ValueError(
+            "Require a label for each dataset, if more than one is provided."
+        )
+
     cm = 1 / 2.54
     fig, ax = plt.subplots(figsize=(width * cm, height * cm))
-    plot_spectra(
-        fig,
-        ax,
-        dset,
-        stations,
-        component,
-        ymin=ymin,
-        ymax=ymax,
-    )
+    setup_plot_styling(ax, component, ymin=ymin, ymax=ymax)
+    for i, dataset_path in enumerate(dataset_paths):
+        dset = xr.open_dataset(dataset_path, engine="h5netcdf")
+        for station in stations:
+            label = station
+            if scenarios is not None and len(scenarios) > 1 and len(stations) > 1:
+                scenario = scenarios[i]
+                label = f"{station} ({scenario})"
+            elif scenarios is not None and len(stations) == 1:
+                label = scenarios[i]
+
+            plot_spectra(
+                fig,
+                ax,
+                dset,
+                [station],
+                component,
+                ymin=ymin,
+                ymax=ymax,
+                labels=[label],
+            )
+
+    if len(stations) * len(dataset_paths) > 1:
+        ax.legend()
 
     if title:
         fig.suptitle(title)
@@ -104,7 +131,3 @@ def plot_spectra_cli(
     else:
         fig.show()
         plt.show()
-
-
-if __name__ == "__main__":
-    app()
